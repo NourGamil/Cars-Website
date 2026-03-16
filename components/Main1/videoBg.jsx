0042f0922c7637f6e073074ca5c16f0ebbadb2e3
NourGamil <69428846+NourGamil@users.noreply.github.com>
2026-03-16 20:57:41 +0200

    Update videoBg.jsx

--- components/Main1/videoBg.jsx
+++ components/Main1/videoBg.jsx
@@ -9,7 +9,7 @@ const VideoBackground = () => {
       max-sm:!left-[50%]
       video-background "
     >
-      <source src="/images/1.mp4" type="video/mp4" />
+      <source src="images/1.mp4" type="video/mp4" />
     </video>
   );
 };
