901e4c31bb74c079384132424c35df7e12cdcb82
NourGamil <69428846+NourGamil@users.noreply.github.com>
2026-03-16 13:10:12 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-
-  basePath: "/Cars-Website",
-  assetPrefix: "/Cars-Website/",
-
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
-
 
 export default nextConfig;
